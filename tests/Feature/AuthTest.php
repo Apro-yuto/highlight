49b0c3fe8fc9b727a60c7b0d6e5bdfc36d0df3f2
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2021-11-08 23:49:36 +0900

    Fix: ログイン機能のtestcase作成

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * ログインページが表示できるか確認。
+     *
+     * @return void
+     */
+    public function testCanDisplayLoginPage()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * 登録済みのユーザーで正常にログインができるか確認。
+     *
+     * @return void
+     */
+    public function testCanLogin()
+    {
+        $user = User::factory()->create();
+
+        $data = [
+            'email'    => $user->email,
+            'password' => 'password',
+        ];
+
+        $this->post('/login', $data)->assertRedirect(route('top'));
+    }
+
+    /**
+     * 登録済みのユーザーで正常にログインができるか確認。
+     *
+     * @return void
+     */
+    public function testCantLogin()
+    {
+        $data = [
+            'email'    => 'helloworld@gmail.com',
+            'password' => 'password',
+        ];
+
+        $this->from('/login')
+              ->post('/login', $data)
+              ->assertRedirect('/login');
+    }
+
+    /**
+     * 認証状態でログインページにアクセス
+     *
+     * @return void
+     */
+    public function testCanAccessTopPage()
+    {
+        $user = User::factory()->make();
+        $this->actingAs($user);
+        $this->assertAuthenticated();
+
+        $this->get('/login')->assertRedirect(route('top'));
+    }
+
+    /**
+     * 非認証時にtopページにアクセス
+     *
+     * @return void
+     */
+    public function testCantAccessTopPage()
+    {
+        $this->assertGuest();
+
+        $this->get(route('top'))->assertRedirect('/login');
+    }
+}
