2c0dc296303822960812fb986e9cdee828fb3dc3
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2021-11-08 23:33:14 +0900

    Fix: routingファイル修正

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -15,12 +16,14 @@ use Inertia\Inertia;
 |
 */
 
-
 Route::middleware('guest')->group(function () {
+    Route::get('/login/redirect', [AuthController::class, 'redirectLoginPage'])->name('login.redirect');
     Route::get('/login', [AuthController::class, 'showLoginPage'])->name('login.index');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
 Route::middleware('auth')->group(function () {
     Route::get('/reg', function () {
-        return Inertia::render('Reg/Index', ['data' => ['yuto', 'hayashi', 'shu', 'sakadume', 'oowada']]);
-    });
+        return Inertia::render('Reg/Index', ['data' => ['yuto', 'hayashi', 'shu', 'sakadume', 'oowada'], 'user' => Auth::user()]);
+    })->name('top');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
