59722221310057f634a274c5e471c6a82cc33c9c
masato5579 <73378197+masato5579@users.noreply.github.com>
2022-01-11 00:50:55 +0900

    [add]topページのmigrationファイル作成

---
+++ database/migrations/2022_01_10_232416_create_items_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('brand_id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('color_id');
+            $table->unsignedBigInteger('label_id');
+            $table->unsignedBigInteger('shop_id');
+            $table->unsignedBigInteger('status_id')->default(1);
+            $table->unsignedBigInteger('supplier_id');
+            $table->text('name');
+            $table->unsignedInteger('gender');
+            $table->text('img_url')->nullable();
+            $table->unsignedInteger('purchase_price');
+            $table->unsignedInteger('selling_price');
+            $table->text('template')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
