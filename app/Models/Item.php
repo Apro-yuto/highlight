2caaefb147d699a75b420a916281a2f4c29c43f9
masato5579 <73378197+masato5579@users.noreply.github.com>
2022-01-12 01:29:46 +0900

    [modify]itemModelリレーション作成

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Faker\Provider\ar_SA\Color;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -55,4 +56,60 @@ class Item extends Model
         'created_at',
         'updated_at',
     ];
+
+    /**
+     * brandを取得
+     */
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    /**
+     * categoryを取得
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * colorsを取得
+     */
+    public function colors()
+    {
+        return $this->belongsToMany(Color::class, 'color_item');
+    }
+
+    /**
+     * lableを取得
+     */
+    public function lable()
+    {
+        return $this->belongsTo(Lable::class);
+    }
+
+    /**
+     * shopを取得
+     */
+    public function shop()
+    {
+        return $this->belongsTo(Shop::class);
+    }
+
+    /**
+     * statusを取得
+     */
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
+
+    /**
+     * supplierを取得
+     */
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
 }
