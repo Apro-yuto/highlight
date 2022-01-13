507590392974687a8bdaffd0ea64b493ec6adbd7
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2022-01-14 02:49:39 +0900

    [Add] Layoutを読み込む記述追加

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,6 +2,7 @@ import { InertiaApp } from '@inertiajs/inertia-react'
 import React from 'react'
 import { render } from 'react-dom'
 import '@/sass/login.scss'
+import Layout from '@/js/Layouts/Layout'
 
 const app = document.getElementById('app')
 const pages = import.meta.glob('./Pages/**/*.tsx')
@@ -10,8 +11,14 @@ render(
   <InertiaApp
     initialPage={JSON.parse(app.dataset.page)}
     resolveComponent={(name) =>
-      pages[`./Pages/${name}.tsx`]().then((module) => module.default)
+      pages[`./Pages/${name}.tsx`]()
+        .then((module) => {
+          const pageDefault = module.default
+          
+          if (pageDefault.layout === undefined) pageDefault.layout = page => <Layout>{page}</Layout>
+          return pageDefault
+        })
     }
   />,
-  app
+  app,
 )
