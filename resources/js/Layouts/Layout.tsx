0a3c7466a328a71d1b81948905e7023f01599b94
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2022-01-18 04:08:13 +0900

    [Add] レイアウト修正

--- resources/js/Layouts/Layout.tsx
+++ resources/js/Layouts/Layout.tsx
@@ -1,5 +1,8 @@
 import React, { ReactNode } from 'react'
+import Box from '@mui/material/Box'
 import Header from '@/js/components/Header'
+import Toolbar from '@mui/material/Toolbar'
+import Sidemenu from '@/js/components/Sidemenu'
 
 interface Props {
   children: ReactNode
@@ -9,7 +12,15 @@ const Layout: React.VFC<Props> = ({ children }) => {
   return (
     <main>
       <Header />
-      <article>{children}</article>
+      <Sidemenu />
+      <Box sx={{ display: 'flex' }}>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Toolbar />
+          <section>
+            {children}
+          </section>
+        </Box>
+      </Box>
     </main>
   )
 }
