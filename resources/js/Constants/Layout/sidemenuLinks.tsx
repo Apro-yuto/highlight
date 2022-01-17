6acf0564e9e83174d32defcc8ec78fe0427ca522
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2022-01-18 04:05:43 +0900

    [Add] サイドメニューのリンクを定数化

---
+++ resources/js/Constants/Layout/sidemenuLinks.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import LocalMallIcon from '@mui/icons-material/LocalMall'
+import LogoutIcon from '@mui/icons-material/Logout'
+import { SidemenuLinks, SidemenuOptions } from '@/js/Types/Layout/Sidemenu'
+
+export const sidemenuLinks: SidemenuLinks[] = [
+  {
+    name: '商品管理',
+    url: '/reg',
+    icon: <LocalMallIcon />,
+  },
+]
+
+export const sidemenuOptions: SidemenuOptions[] = [
+  {
+    name: 'ログアウト',
+    url: '/logout',
+    method: 'post',
+    icon: <LogoutIcon />,
+  },
+]
+
+export default undefined
