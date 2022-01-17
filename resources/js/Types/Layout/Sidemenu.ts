41b1c8947b47faf84545f6a8fb966b19d4f24112
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2022-01-18 04:06:29 +0900

    [Add] サイドメニューのプロパティに対して、型を定義

---
+++ resources/js/Types/Layout/Sidemenu.ts
@@ -0,0 +1,11 @@
+export interface SidemenuLinks {
+  name: string,
+  url: string,
+  icon: React.ReactNode
+}
+
+export interface SidemenuOptions extends SidemenuLinks {
+  method: string 
+}
+
+export default undefined
