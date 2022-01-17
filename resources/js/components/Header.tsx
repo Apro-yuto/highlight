593076ac5d9addd38703ac02f61219496d741e42
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2022-01-18 04:08:37 +0900

    [Fix] ヘッダーコンポーネント修正

--- resources/js/components/Header.tsx
+++ resources/js/components/Header.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Link } from '@inertiajs/inertia-react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Box from '@mui/material/Box'
 import Styles from '@/sass/Layout/Header.module.scss'
 import IconButton from '@mui/material/IconButton'
 import Button from '@mui/material/Button'
@@ -10,31 +8,23 @@ import MenuIcon from '@mui/icons-material/Menu'
 
 const Header: React.VFC = () => {
   return (
-    <Box>
-      <AppBar position="static">
+      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar className={Styles.appBar} variant="dense">
-          <Button>
+          <Button sx={{ padding: '0 1rem' }}>
             <a className={Styles.title} href="/reg">
               HIGHLIGHT
             </a>
-          </Button>
-          <Button sx={{ ml: 'auto' }}>
-            <Link className={Styles.link} as="p" href="/logout" method="post">
-              LOGOUT
-            </Link>
           </Button>
           <IconButton
             className={Styles.icon}
             size="medium"
             edge="start"
             aria-label="menu"
-            sx={{ ml: 'auto' }}
-          >
+            sx={{ ml: 'auto' }}>
             <MenuIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
-    </Box>
   )
 }
 
