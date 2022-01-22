30ecbda54924eec345cb96d1d8347d21427a18ed
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2022-01-22 14:45:52 +0900

    [Fix] Layout修正

--- resources/js/Layouts/Layout.tsx
+++ resources/js/Layouts/Layout.tsx
@@ -1,24 +1,23 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState } from 'react'
 import Box from '@mui/material/Box'
 import Header from '@/js/components/Header'
 import Toolbar from '@mui/material/Toolbar'
 import Sidemenu from '@/js/components/Sidemenu'
 
-interface Props {
-  children: ReactNode
-}
+const Layout: React.VFC<{ children: ReactNode }> = ({ children }) => {
+  const [isOpen, toggleIsOpen]: [
+    boolean,
+    React.Dispatch<React.SetStateAction<boolean>>,
+  ] = useState<boolean>(false)
 
-const Layout: React.VFC<Props> = ({ children }) => {
   return (
     <main>
-      <Header />
-      <Sidemenu />
+      <Header toggleIsOpen={() => toggleIsOpen(!isOpen)} />
       <Box sx={{ display: 'flex' }}>
+        <Sidemenu isOpen={isOpen} />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
           <Toolbar />
-          <section>
-            {children}
-          </section>
+          <section>{children}</section>
         </Box>
       </Box>
     </main>
