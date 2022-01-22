d833dcd91fd0534fb4b1cf845d4a8bae44cf0b9f
masato5579 <73378197+masato5579@users.noreply.github.com>
2022-01-23 04:38:26 +0900

    [fix]タイポ修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            itemSeeder::class,
+            ItemSeeder::class,
             BaseTemplateSeeder::class,
             BrandSeeder::class,
             CategorySeeder::class,
