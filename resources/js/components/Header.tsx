4ee718c24c9f501cfbaddf5f2131f5c45388bb1a
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2022-01-22 14:46:08 +0900

    [Fix] Headerコンポーネント修正

--- resources/js/components/Header.tsx
+++ resources/js/components/Header.tsx
@@ -6,25 +6,30 @@ import IconButton from '@mui/material/IconButton'
 import Button from '@mui/material/Button'
 import MenuIcon from '@mui/icons-material/Menu'
 
-const Header: React.VFC = () => {
+const Header: React.VFC<{ toggleIsOpen: () => void }> = ({ toggleIsOpen }) => {
   return (
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar className={Styles.appBar} variant="dense">
-          <Button sx={{ padding: '0 1rem' }}>
-            <a className={Styles.title} href="/reg">
-              HIGHLIGHT
-            </a>
-          </Button>
-          <IconButton
-            className={Styles.icon}
-            size="medium"
-            edge="start"
-            aria-label="menu"
-            sx={{ ml: 'auto' }}>
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+    <AppBar
+      position="fixed"
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    >
+      <Toolbar className={Styles.appBar} variant="dense">
+        <Button sx={{ padding: '0 1rem' }}>
+          <a className={Styles.title} href="/reg">
+            HIGHLIGHT
+          </a>
+        </Button>
+        <IconButton
+          onClick={toggleIsOpen}
+          className={Styles.icon}
+          size="medium"
+          edge="start"
+          aria-label="menu"
+          sx={{ ml: 'auto' }}
+        >
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   )
 }
 
