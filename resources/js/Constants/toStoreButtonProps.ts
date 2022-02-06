7d20175d9470929b374b9c108d92f077c1b58eff
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2022-02-07 02:21:46 +0900

    [Add] 商品登録ページへのリンク実装

---
+++ resources/js/Constants/toStoreButtonProps.ts
@@ -0,0 +1,25 @@
+import { SxProps } from '@mui/system'
+
+export const FabStyle: SxProps = {
+  width: {
+    xs: 58,
+    lg: 70,
+  },
+  height: {
+    xs: 58,
+    lg: 70,
+  },
+  position: 'fixed',
+  bgcolor: 'common.white',
+  border: '3px solid #AFDFE4',
+  bottom: {
+    xs: 50,
+    lg: 100,
+  },
+  right: {
+    xs: 25,
+    lg: 100,
+  },
+}
+
+export default undefined
