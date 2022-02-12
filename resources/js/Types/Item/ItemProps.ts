076fc713fd02ef70c56224baf7064a4983b58bb2
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2022-02-13 05:43:52 +0900

    [Add] itemの型定義

---
+++ resources/js/Types/Item/ItemProps.ts
@@ -0,0 +1,13 @@
+interface ItemProps {
+  id: number
+  itemName: string
+  itemImage: string
+  status: string
+  brand: string
+  category: string
+  gender: number
+  purchasePrice: string
+  sellingPrice: string
+}
+
+export default ItemProps
