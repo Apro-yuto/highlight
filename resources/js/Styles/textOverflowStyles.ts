c32ac7c2a2a9b5f0115839bba6e458ac826a7ccb
Apro-yuto <84962399+Apro-yuto@users.noreply.github.com>
2022-02-13 05:45:52 +0900

    [Add] textOverflowに使用するスタイル定義

---
+++ resources/js/Styles/textOverflowStyles.ts
@@ -0,0 +1,10 @@
+import { SxProps } from '@mui/system'
+
+const textOverflowStyles: SxProps = {
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+  maxWidth: '100%',
+}
+
+export default textOverflowStyles
